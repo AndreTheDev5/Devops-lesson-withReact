332a8ba51c239fa292e95c778b35b2a1e8242c6a
Andrea Loprete <andrethedev5@gmail.com>
2023-10-03 17:34:25 +0200

    remove reactstrictmode from nextconfig

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
   // Configurazione NEXT per siti statici con la possibilità di HOST su GitHub Actions/Pages
   output:"export",
   basePath:"/Devops-lesson.withReact",  //Nome repository GitHub 
