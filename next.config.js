23f953f504f9f5aa4e838382b072c68ad2b0aac6
Andrea Loprete <andrethedev5@gmail.com>
2023-10-03 15:10:27 +0200

    Ho aggiunto lo step build e la Pipeline con GitHub Actions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // Configurazione NEXT per siti statici con la possibilità di HOST su GitHub Actions/Pages
+  output:"export",
+  basePath:"/Devops-lesson.withReact",  //Nome repository GitHub 
+  images: {
+    unoptimized:true
+  }
 }
 
 module.exports = nextConfig
